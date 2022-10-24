1494918a7d85dc1bc3eba4bfc9368b74b318cd0b
Daniel Cook <brikbusters@gmail.com>
2022-10-23 19:29:30 -0700

    JNI code

--- src/jni.c
+++ src/jni.c
@@ -13,29 +13,25 @@ struct PtpRuntime ptp_runtime;
 #endif
 
 struct AndroidBackend {
-	int in;
-	int out;
+    jobject in;
+    jobject out;
+    jclass pac;
+    JNIEnv *env;
 }backend;
 
-jmethodID jni_bulktransfer;
-
-JNI_FUNC(void, init)(JNIEnv *env, jobject thiz) {
+JNI_FUNC(void, Cinit)(JNIEnv *env, jobject thiz, jobject pac) {
 	// TODO: init piclib
 	ptp_runtime.transaction = 1;
 	ptp_runtime.session = 0;
 	ptp_runtime.data = malloc(4096);
 	ptp_runtime.data_length = 4096;
 
-	jclass clsCallback = env->FindClass("se/m7n/android/libusb/LibUsb$Callback");
-	jni_bulktransfer = env->GetMethodID(clsCallback, "bulkTransfer", "(Landroid/hardware/usb/UsbDeviceConnection;Landroid/hardware/usb/UsbEndpoint;[BII)I");
+    backend.pac = (*env)->GetObjectClass(env, pac);
+    backend.env = env;
 }
 
-JNI_FUNC(void setEndpoints)(JNIEnv *env, jobject thiz, jint in, jint out) {
-	backend.in = (int)in;
-	backend.out = (int)out;
-}
-
-JNI_FUNC(jbyteArray, recievePacket)(JNIEnv *env, jobject thiz, jint code, jintArray params0, jint read_size) {
+#if 0
+JNI_FUNC(jbyteArray, CrecievePacket)(JNIEnv *env, jobject thiz, jint code, jintArray params0, jint read_size) {
 	jsize length = (*env)->GetArrayLength(env, params0);
 	jint *params1 = (*env)->GetIntArrayElements(env, params0, 0);
 
@@ -53,15 +49,38 @@ JNI_FUNC(jbyteArray, recievePacket)(JNIEnv *env, jobject thiz, jint code, jintAr
 	(*env)->SetByteArrayRegion(env, ret, 0, size, (const jbyte *)(ptp_runtime.data));
 	return ret;
 }
+#endif
 
+JNI_FUNC(jbyteArray, bulkPacketCmd)(JNIEnv *env, jobject thiz, jint code, jintArray params0, jint read_size) {
+    jsize length = (*env)->GetArrayLength(env, params0);
+    jint *params1 = (*env)->GetIntArrayElements(env, params0, 0);
 
-JNI_FUNC(jbyteArray, recievePacketPre)(JNIEnv *env, jobject thiz, jint code) {
-	int size = ptp_recv_packet_pre(&ptp_runtime, code);
+    struct PtpCommand cmd;
+    cmd.code = code;
+    cmd.data_length = 0;
 
-	jbyteArray ret = (*env)->NewByteArray(env, size);
-	(*env)->SetByteArrayRegion(env, ret, 0, size, (const jbyte *)(ptp_runtime.data));
-	return ret;
+    uint32_t params[5];
+
+    for (int i = 0; i < (int)length; i++) {
+        cmd.params[i] = params1[i];
+    }
+
+    cmd.param_length = 0;
+
+    (*env)->ReleaseIntArrayElements(env, params0, params1, 0);
+
+    int size = ptp_bulk_packet_cmd(&ptp_runtime, &cmd);
+
+    jbyteArray ret = (*env)->NewByteArray(env, size);
+    (*env)->SetByteArrayRegion(env, ret, 0, size, (const jbyte *)(ptp_runtime.data));
+    return ret;
 }
 
-//void 
-//(*env)->GetObjectClass(env, foo_obj)
+int ptp_send_bulk_packets(struct PtpRuntime *r, int length) {
+    jbyteArray data = (*backend.env)->NewByteArray(backend.env, length);
+    (*backend.env)->SetByteArrayRegion(backend.env, data, 0, length, (const jbyte *)(ptp_runtime.data));
+
+    jmethodID sendData = (*backend.env)->GetMethodID(backend.env, backend.pac, "sendData", "[BII)I");
+    return (*backend.env)->CallIntMethod(backend.env, backend.pac, data, length, 1000);
+}
+// https://github.com/mikma/androidsc-usb/blob/master/jni/usbjni.cpp
