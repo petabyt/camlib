e15e965d41ae99a65bd21f72ca6af39a1e904d1c
Daniel C <brikbusters@gmail.com>
2023-08-10 13:46:20 -0400

    Remove demented wrong parameter for mutex functions

--- src/util.c
+++ src/util.c
@@ -16,6 +16,7 @@ void ptp_generic_reset(struct PtpRuntime *r) {
 	r->data_phase_length = 0;
 	r->di = NULL;
 	r->connection_type = PTP_USB;
+	r->caller_unlocks_mutex = 0;
 }
 
 void ptp_generic_init(struct PtpRuntime *r) {
