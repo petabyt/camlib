ceff044b2584ebc3d172380321b6c47d4c5662c8
petabyt <petabyt@users.noreply.github.com>
2024-07-15 02:20:12 +0000

    deploy: 01e456551fb1d94727bff18dd0ec62cabf7713ef

--- search/all_1.js
+++ search/all_1.js
@@ -4,9 +4,8 @@ var searchData=
   ['camlib_1',['camlib',['../index.html',1,'']]],
   ['camlib_2eh_2',['camlib.h',['../camlib_8h.html',1,'']]],
   ['chdk_20ptp_20extension_3',['CHDK PTP Extension',['../md_docs_2chdk.html',1,'']]],
-  ['checklist_4',['Checklist',['../index.html#autotoc_md2',1,'']]],
-  ['cl_5fops_2eh_5',['cl_ops.h',['../cl__ops_8h.html',1,'']]],
-  ['comm_5fbackend_6',['comm_backend',['../structPtpRuntime.html#a536ce5f5409e194a56c006fe8bb3d454',1,'PtpRuntime']]],
-  ['command_20to_20the_20device_7',['To issue a command to the device',['../md_docs_2ptp.html#autotoc_md17',1,'']]],
-  ['connection_5ftype_8',['connection_type',['../structPtpRuntime.html#aed36a8eae0146cdb13a2fb5e7ca201a0',1,'PtpRuntime']]]
+  ['cl_5fops_2eh_4',['cl_ops.h',['../cl__ops_8h.html',1,'']]],
+  ['comm_5fbackend_5',['comm_backend',['../structPtpRuntime.html#a536ce5f5409e194a56c006fe8bb3d454',1,'PtpRuntime']]],
+  ['command_20to_20the_20device_6',['To issue a command to the device',['../md_docs_2ptp.html#autotoc_md17',1,'']]],
+  ['connection_5ftype_7',['connection_type',['../structPtpRuntime.html#aed36a8eae0146cdb13a2fb5e7ca201a0',1,'PtpRuntime']]]
 ];
--- search/all_b.js
+++ search/all_b.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['response_5fwait_5fdefault_0',['response_wait_default',['../structPtpRuntime.html#a37501cfcd57c6d29d2201bccfa822d0e',1,'PtpRuntime']]]
+  ['response_5fwait_5fdefault_0',['response_wait_default',['../structPtpRuntime.html#a37501cfcd57c6d29d2201bccfa822d0e',1,'PtpRuntime']]],
+  ['roadmap_1',['Roadmap',['../index.html#autotoc_md2',1,'']]]
 ];
