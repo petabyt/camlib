500c62294f4cbe1449b7218c1d213ecf224c7f16
petabyt <petabyt@users.noreply.github.com>
2024-11-17 22:19:46 +0000

    deploy: abf1aec152907e9b0175fb4f89d8041a4d921590

--- search/classes_0.js
+++ search/classes_0.js
@@ -3,6 +3,7 @@ var searchData=
   ['ptparray_0',['PtpArray',['../structPtpArray.html',1,'']]],
   ['ptpcommand_1',['PtpCommand',['../structPtpCommand.html',1,'']]],
   ['ptpgenericevent_2',['PtpGenericEvent',['../structPtpGenericEvent.html',1,'']]],
-  ['ptppropavail_3',['PtpPropAvail',['../structPtpPropAvail.html',1,'']]],
-  ['ptpruntime_4',['PtpRuntime',['../structPtpRuntime.html',1,'']]]
+  ['ptpliveviewparams_3',['PtpLiveviewParams',['../structPtpLiveviewParams.html',1,'']]],
+  ['ptppropavail_4',['PtpPropAvail',['../structPtpPropAvail.html',1,'']]],
+  ['ptpruntime_5',['PtpRuntime',['../structPtpRuntime.html',1,'']]]
 ];
